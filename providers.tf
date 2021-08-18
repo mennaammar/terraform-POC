a3fd6a1e7d72291b7b603aee7547067471e80eee
Mennatallah Abdallah <mennatallah.abdallah@ibm.com>
2021-08-18 19:35:28 +0200

    workflow add fixed

--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "aws" {
   region = "eu-west-1"
-
+  access_key ="AKIATUUMKTR3R7NP6KUB"
+  secret_key = "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"
 }
--- versions.tf
+++ versions.tf
@@ -2,6 +2,13 @@ terraform {
   required_version = ">= 0.12.6"
 
   required_providers {
-    aws = ">= 3.24"
+    #aws = ">= 3.24"
+    aws = ">= 3.36"
   }
+    backend "s3" {
+      encrypt = true
+       region = "eu-west-1"
+      bucket = "terraform-state-maf-demo"
+      key = "terraform.tfstate"
+    }
 }
