0b649948f675227befa8100f40bcddf2a181b0ef
Mennatallah Abdallah <mennatallah.abdallah@ibm.com>
2021-08-22 11:57:01 +0200

    modification

--- main.tf
+++ main.tf
@@ -45,18 +45,17 @@ module "s3_bucket" {
 
   ##################################################
   ##########Security bugs #################
-  #  block_public_policy = true
-  #   block_public_acls = true
- # server_side_encryption_configuration = {
- #   rule = {
-  #    apply_server_side_encryption_by_default = {
-  #     sse_algorithm = "AES256"
-  #    }
-  #  } 
-   #}
+    block_public_policy = true
+     block_public_acls = true
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+       sse_algorithm = "AES256"
+      }
+    } 
+   }
   
-    #http_endpoint="disabled"
-	  #http_tokens = "required"
+   
 
   
  #####################################################
