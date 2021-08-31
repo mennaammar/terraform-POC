4905cfc4b1bb7e37315cd85b13ecc75b52bb637b
Mennatallah Abdallah <mennatallah.abdallah@ibm.com>
2021-08-31 09:35:20 +0200

    security issues

--- main.tf
+++ main.tf
@@ -58,18 +58,18 @@ module "s3_bucket" {
 
   ##################################################
   ##########Security bugs #################
-    block_public_policy = true
-     block_public_acls = true
-     restrict_public_buckets = true
-     ignore_public_acls = true
+  #   block_public_policy = true
+  #    block_public_acls = true
+  #    restrict_public_buckets = true
+  #    ignore_public_acls = true
      
-     server_side_encryption_configuration = {
-      rule = {
-        apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-        }
-    } 
-   }
+  #    server_side_encryption_configuration = {
+  #     rule = {
+  #       apply_server_side_encryption_by_default = {
+  #       sse_algorithm = "AES256"
+  #       }
+  #   } 
+  #  }
   
    logging = {
 		target_bucket = "bucket-logging-maf-us-east" #logging-bucketmaf
